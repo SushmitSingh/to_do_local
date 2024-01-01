60ae633792b19cfda9e066466ef93811ea7f6824
Sushmit Singh <sushmit8998@gmail.com>
2024-01-01 15:58:29 +0530

    Have To fix Local data

--- lib/bin/server.dart
+++ lib/bin/server.dart
@@ -1,19 +1,24 @@
 import 'dart:convert';
 
-import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-final todos = <String>[];
+final List<String> todos = [];
 
-Response getAllTodos(Request request) {
-  final jsonResponse = jsonEncode({'todos': todos});
-  return Response.ok(jsonResponse,
-      headers: {'Content-Type': 'application/json'});
+shelf.Response getAllTodos(shelf.Request request) {
+  try {
+    final jsonResponse = jsonEncode({'todos': todos});
+    return shelf.Response.ok(jsonResponse,
+        headers: {'Content-Type': 'application/json'});
+  } catch (error) {
+    print('Error getting todos: $error');
+    return shelf.Response.internalServerError(body: 'Error getting todos');
+  }
 }
 
-Future<Response> addTodo(Request request) async {
+Future<shelf.Response> addTodo(shelf.Request request) async {
   try {
     // Read the request body
     final requestBody = await request.readAsString();
@@ -28,11 +33,11 @@ Future<Response> addTodo(Request request) async {
     todos.add(task);
 
     // Respond with success
-    return Response.ok('Todo added');
+    return shelf.Response.ok('Todo added');
   } catch (error) {
     // Handle errors
     print('Error adding todo: $error');
-    return Response.internalServerError(body: 'Error adding todo');
+    return shelf.Response.internalServerError(body: 'Error adding todo');
   }
 }
 
@@ -42,7 +47,7 @@ void main() {
   app.get('/todos', getAllTodos);
   app.post('/addTodo', addTodo);
 
-  var handler = const Pipeline()
+  var handler = const shelf.Pipeline()
       .addMiddleware(corsHeaders(
         headers: {
           'Access-Control-Allow-Origin': '*',
@@ -50,10 +55,10 @@ void main() {
           'Access-Control-Allow-Headers': 'Origin, Content-Type',
         },
       ))
-      .addMiddleware(logRequests())
+      .addMiddleware(shelf.logRequests())
       .addHandler(app);
 
-  shelf_io.serve(handler, '192.168.137.1', 8080).then((server) {
-    print('Server started on port 8080');
+  shelf_io.serve(handler, '192.168.137.1', 3000).then((server) {
+    print('Server started on port 3000');
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+import 'package:sembast_web/sembast_web.dart';
 
 void main() => runApp(const MyApp());
 
@@ -21,41 +27,159 @@ class MyApp extends StatelessWidget {
 }
 
 class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({Key? key});
+
   @override
   _TodoListScreenState createState() => _TodoListScreenState();
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
   List<String> todos = [];
+  late String storePath;
 
   @override
   void initState() {
     super.initState();
-    // Fetch todos from the server when the screen initializes
-    _fetchTodos();
+    _initStorePath();
+    _fetchLocalTodos();
+    _processOfflineTodos();
   }
 
-  Future<void> _fetchTodos() async {
-    try {
-      // Make a GET request to fetch todos from the server
-      var response = await http.get(
-        Uri.parse('http://192.168.137.1:8080/todos'),
-      );
+  Future<void> _initStorePath() async {
+    final appDocumentsDirectory = await getApplicationDocumentsDirectory();
+    storePath = '${appDocumentsDirectory.path}/todos_store.db';
+  }
 
-      if (response.statusCode == 200) {
-        // If the server returns a 200 OK response, update the local todos list
-        setState(() {
-          // Parse the JSON response to get the 'todos' list
-          final Map<String, dynamic> data = jsonDecode(response.body);
-          todos = List<String>.from(data['todos']);
-        });
-      } else {
-        // If the server returns an error response, handle it accordingly
-        print('Failed to fetch todos. Server returned ${response.statusCode}');
+  Future<void> _fetchLocalTodos() async {
+    try {
+      final DatabaseFactory dbFactory =
+          kIsWeb ? databaseFactoryWeb : databaseFactoryIo;
+      final Database db = await dbFactory.openDatabase(storePath);
+
+      final store = stringMapStoreFactory.store('todos');
+      final record = await store.record('todos').get(db);
+
+      if (record != null) {
+        final Map<String, dynamic> data = record;
+        if (data.containsKey('todos')) {
+          setState(() {
+            todos.clear();
+            todos.addAll(List<String>.from(data['todos']));
+          });
+        }
       }
     } catch (error) {
-      // Handle network-related errors
-      print('Error fetching todos: $error');
+      print('Error fetching local todos: $error');
+    }
+  }
+
+  Future<void> _saveLocalTodos(List<String> todosToSave) async {
+    try {
+      final DatabaseFactory dbFactory =
+          kIsWeb ? databaseFactoryWeb : databaseFactoryIo;
+      final Database db = await dbFactory.openDatabase(storePath);
+
+      final store = stringMapStoreFactory.store('todos');
+      await store.record('todos').put(db, {'todos': todosToSave});
+    } catch (error) {
+      print('Error saving local todos: $error');
+    }
+  }
+
+  Future<void> _saveTodoOnServer(String task) async {
+    try {
+      // Save locally first
+      await _saveLocalTodos([...todos, task]);
+
+      Map<String, dynamic> newTodo = {'task': task};
+
+      if (await _checkInternetConnection()) {
+        String todoJson = jsonEncode(newTodo);
+
+        print('Sending todo to server: $todoJson');
+
+        var response = await http.post(
+          Uri.parse(
+              'http://192.168.137.1:3000/addTodo'), // Replace with your server endpoint
+          headers: {'Content-Type': 'application/json'},
+          body: todoJson,
+        );
+
+        print('Server response status code: ${response.statusCode}');
+        print('Server response body: ${response.body}');
+
+        if (response.statusCode == 200) {
+          // If the server returns a 200 OK response, fetch the updated todos
+          await _syncWithServer();
+        } else {
+          print(
+              'Failed to add todo on the server. Server returned ${response.statusCode}');
+        }
+      } else {
+        print('No internet connection. Saving todo locally.');
+      }
+    } catch (error) {
+      print('Error sending todo to the server: $error');
+    }
+  }
+
+  Future<void> _syncWithServer() async {
+    try {
+      var response = await http.get(
+        Uri.parse(
+            'http://192.168.137.1:3000/todos'), // Replace with your server endpoint
+      );
+
+      print('Response status code: ${response.statusCode}');
+      print('Response headers: ${response.headers}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+
+        setState(() {
+          todos = List<String>.from(data['todos']);
+        });
+
+        await _saveLocalTodos(todos);
+      } else {
+        print(
+            'Failed to fetch todos from the server. Server returned ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error syncing with server: $error');
+    }
+  }
+
+  Future<void> _processOfflineTodos() async {
+    try {
+      final DatabaseFactory dbFactory =
+          kIsWeb ? databaseFactoryWeb : databaseFactoryIo;
+      final Database db = await dbFactory.openDatabase(storePath);
+
+      final store = stringMapStoreFactory.store('offlineTodos');
+      final todosToProcess = await store.find(db);
+
+      for (var todoRecord in todosToProcess) {
+        final String task = todoRecord['task'] as String;
+        await _saveTodoOnServer(task);
+      }
+
+      // After processing, delete only the processed records
+      for (var todoRecord in todosToProcess) {
+        await store.record(todoRecord.key).delete(db);
+      }
+    } catch (error) {
+      print('Error processing offline todos: $error');
+    }
+  }
+
+  Future<bool> _checkInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
     }
   }
 
@@ -84,6 +208,15 @@ class _TodoListScreenState extends State<TodoListScreen> {
                 _addTodo();
               },
               child: Text('Add Todo'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                _syncWithServer();
+              },
+              child: Text('Sync with Server'),
             ),
           ),
         ],
@@ -115,41 +248,13 @@ class _TodoListScreenState extends State<TodoListScreen> {
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
-                _saveTodo(_textController.text);
+                _saveTodoOnServer(_textController.text);
               },
-              child: Text('Save'),
+              child: Text('Save Locally'),
             ),
           ],
         );
       },
     );
-  }
-
-  Future<void> _saveTodo(String task) async {
-    try {
-      // Create a Map representing the new todo
-      Map<String, dynamic> newTodo = {'task': task};
-
-      // Convert the Map to a JSON string
-      String todoJson = jsonEncode(newTodo);
-
-      // Make a POST request to the server
-      var response = await http.post(
-        Uri.parse('http://192.168.137.1:8080/addTodo'),
-        headers: {'Content-Type': 'application/json'},
-        body: todoJson,
-      );
-
-      if (response.statusCode == 200) {
-        // If the server returns a 200 OK response, fetch the updated todos
-        _fetchTodos();
-      } else {
-        // If the server returns an error response, handle it accordingly
-        print('Failed to add todo. Server returned ${response.statusCode}');
-      }
-    } catch (error) {
-      // Handle network-related errors
-      print('Error sending todo: $error');
-    }
   }
 }
